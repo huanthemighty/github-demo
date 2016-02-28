c5592f574e9900745ee800567028cd51d3276f83
huanthemighty <huan.sun@gmail.com>
2016-02-27 21:30:19 -0800

    Import the git-demo project

---
+++ src/main/java/com/huanthemighty/App.java
@@ -0,0 +1,13 @@
+package com.huanthemighty;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
