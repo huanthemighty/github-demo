a798f3d05460191701a47d55e8c96444f1d9c9df
huanthemighty <huan.sun@gmail.com>
2016-02-28 21:45:23 -0800

    add dagger2 example

---
+++ src/main/java/coffee/CoffeeApp.java
@@ -0,0 +1,18 @@
+package coffee;
+
+import coffee.DaggerCoffeeApp_Coffee;
+import dagger.Component;
+import javax.inject.Singleton;
+
+public class CoffeeApp {
+  @Singleton
+  @Component(modules = { DripCoffeeModule.class })
+  public interface Coffee {
+    CoffeeMaker maker();
+  }
+
+  public static void main(String[] args) {
+    Coffee coffee = DaggerCoffeeApp_Coffee.builder().build();
+    coffee.maker().brew();
+  }
+}
